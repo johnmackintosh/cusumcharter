0277a236baf3572daefb81e4b8b25c2a8310ba2e
John MacKintosh <johnmackintosh@gmx.com>
2021-08-22 22:45:47 +0100

    docs and cusum control

--- R/cusum_control.R
+++ R/cusum_control.R
@@ -2,12 +2,15 @@
 #'
 #' @param x  input vector
 #' @param target  target value for comparison, the mean of x will be used if missing
-#' @param std_dev target value for standard deviation, the screened moving range of x will be used if missing
-#' @param desired_shift how many standard deviations do you want to detect, between 0.5 and 1 is the usual default
-#' @param k allowable slack - defaults to half of standard deviation
-#' @param h control limits usually between 4 and 5
+#' @param std_dev  Defaults to the screened moving range of x.
+#' A known or desired value for standard deviation can be supplied instead.
+#' @param desired_shift how many standard deviations do you want to detect?
+#' This value is typically between  0.5 to 1. Defaults to 1.
+#' @param k allowable slack - defaults to half the standard deviation multiplied by desired shift
+#' @param h action limits -  usually between 4 and 5, defaults to 4.
+#' The standard deviation is multiplied by this value to determine the upper and lower limits on the chart
 #'
-#' @return dataframe showing original inputs and calculated control limits
+#' @return data.frame showing original inputs and calculated control limits
 #' @export
 #'
 #' @examples
@@ -20,14 +23,14 @@ cusum_control <- function(x,
                           std_dev = NULL,
                           desired_shift = 1,
                           k = 0.5,
-                          h = 5){
+                          h = 4){
 
   # Variables
   target <-  if (is.null(target)) {target = mean(x, na.rm = TRUE)} else target
   mr <- mean(abs(diff(x)), na.rm = TRUE)
   std_dev <- if (is.null(std_dev)) {std_dev = mr / 1.128} else std_dev
-  k <- if (is.null(k)) {k = 0.5 * std_dev} else (std_dev * k)
-  h <- if (is.null(h)) {5} else h
+  k <- if (is.null(k)) {k = (0.5 * std_dev) * desired_shift} else (std_dev * k) * desired_shift
+  h <- if (is.null(h)) {4} else h
 
   ucl <- target + k
   lcl <- target - k
@@ -42,7 +45,8 @@ cusum_control <- function(x,
 
 
     for (i in 2:length(x)) {
-      pos[i] <- max(0,(x[i] - limit + pos[i - 1] ))
+     pos[i] <- max(0,(x[i] - limit + pos[i - 1] ))
+
     }
     pos
 
@@ -53,7 +57,7 @@ cusum_control <- function(x,
 
 
     for (i in 2:length(x)) {
-      pos[i] <-  max(0,(limit - x[i] + pos[i - 1]))
+      pos[i] <-  max(0,(limit - x[i] + pos[i-1]))
     }
     pos
 
@@ -68,7 +72,7 @@ cusum_control <- function(x,
     x[1] <- compar_col[1]
 
     for (i in 2:length(compar_col)) {
-      if (compar_col[i] == 0){
+      if (compar_col[i] == 0) {
         x[i] =  0
       } else {
         x[i] =  x[i - 1] + compar_col[i]
@@ -84,10 +88,10 @@ cusum_control <- function(x,
   variance <- x - target
   cusum <- cumsum(variance)
 
-  x_ucl <- x - ucl
+
   cplus <- rep(0,length(x))
   cplus[1] <- max(0,(x[1] - ucl[1] + 0))
-  x_lcl <- lcl - x
+
   cneg <- rep(0,length(x))
   cneg[1] <- max(0,(lcl - x[1] + 0))
 
@@ -107,7 +111,8 @@ cusum_control <- function(x,
   cneg <- cneg * -1
 
 
-  out_df <- data.frame(x,target, variance,cusum,cplus,cum_nplus,cneg,cum_nneg)
+  out_df <- data.frame(x, target, variance, std_dev, cusum, cplus, cneg,
+                       cum_nplus, cum_nneg)
   out_df$ucl <- h * std_dev
   out_df$lcl <- (h * std_dev) * -1
   out_df$centre <- 0L
